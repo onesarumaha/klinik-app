011f0d8d438bee156d01212c4a6577d0181e1f8e
one sarumaha <onesarumaha10@gmail.com>
2026-01-30 16:20:10 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\ObatController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PatientController;
-use App\Models\Patient;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,14 +18,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // master patients
-    Route::resource('patients', PatientController::class);
 
     // master users
     Route::get('/users', [ProfileController::class, 'index'])->name('user.index');
+    Route::get('/patient', [PatientController::class, 'index'])->name('patients.index');
 
     // master data obat 
     Route::get('/data-obat', [ObatController::class, 'index'])->name('data-obat');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
